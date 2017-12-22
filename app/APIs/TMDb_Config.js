4e56430c3a5f023807b1cbcae36a96988e6e4b11
nhan <thiennhank9@gmail.com>
2017-12-22 21:07:37 +0700

    change structure code a liitle with APIs

--- app/APIs/TMDb_Config.js
+++ app/APIs/TMDb_Config.js
@@ -1,7 +1,19 @@
-export default api = {
+api_config = {
     key: '0f866d616e28d66616b042c3c43a39d4',
-    request_token: 'b305e1ceb1242ad45945562d8aefd2e90ffd88b5',
-    session_id: '252a2234321a4b3bb3b480685c02d82e39cd0fc2',
     version: '3',
-    url: 'https://api.themoviedb.org/' + this.version,
+    width_image: 'w500',
+    url_no_version: 'https://api.themoviedb.org/',
+    url_base_image_no_width: 'https://image.tmdb.org/t/p/',
+}
+
+api_urls = {
+    url_main: 'https://api.themoviedb.org/3',
+    url_base_image: 'https://image.tmdb.org/t/p/w500',
+}
+
+export default api = {
+    url_request_top_popularity: 'https://api.themoviedb.org/3/discover/movie?api_key='+ api_config.key + '&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1',
+    url_get_image: (path) => {
+        return api_urls.url_base_image + path
+    }
 }
--- app/Screens/Loading.js
+++ app/Screens/Loading.js
@@ -4,26 +4,20 @@ import { PulseIndicator } from 'react-native-indicators';
 import StatusBarApp from '../Components/StatusBarApp.js';
 import styles from './Styles/Loading.js';
 import actionCreators from '../Redux/ActionsCreator.js';
-import { NavigationActions } from 'react-navigation';
+import {resetAction} from '../Navigators/NavigationActions.js';
+import api from '../APIs/TMDb_Config.js';
 
-const url_request = 'https://api.themoviedb.org/3/discover/movie?api_key=0f866d616e28d66616b042c3c43a39d4&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=true&page=1';
-const url_base_image = 'https://image.tmdb.org/t/p/w500';
+// const url_request = 'https://api.themoviedb.org/3/discover/movie?api_key=0f866d616e28d66616b042c3c43a39d4&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=true&page=1';
+// const url_base_image = 'https://image.tmdb.org/t/p/w500';
 
 //config reset action navigation 
-const resetAction = NavigationActions.reset({
-    index: 0,
-    actions: [
-        NavigationActions.navigate({ routeName: 'TabApp' })
-    ]
-})
-
 export default class Loading extends Component {
     constructor(props) {
         super(props);
     }
 
     getListImagesFilmPopularity() {
-        return fetch(url_request)
+        return fetch(api.url_request_top_popularity)
             .then(response => response.json())
             .then(responseJson => {
                 let results = responseJson.results;
@@ -35,7 +29,7 @@ export default class Loading extends Component {
                     let objElement = {
                         //get field from json, can add/edit fields that is needeed here, example json can see in https://developers.themoviedb.org/3/discover/movie-discover
                         key: index,
-                        uri: url_base_image + element.backdrop_path,
+                        uri: api.url_get_image(element.backdrop_path),
                         title: element.title,
                     }
 
@@ -43,8 +37,6 @@ export default class Loading extends Component {
                     list_images.push(objElement);
                 });
 
-                console.log('LIST_IMAGES_POPULARITY: ')
-                console.log(list_images);
                 //set state list image
                 store.dispatch(actionCreators.add_list_top_popularity(list_images));
 
