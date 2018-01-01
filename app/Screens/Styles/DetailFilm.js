c908f42abe2292b9ec6b71f7f23586c7a7f2ee98
Satigo <satigo@github.com>
2018-01-01 23:47:59 +0700

    change position play button

--- app/Screens/Styles/DetailFilm.js
+++ app/Screens/Styles/DetailFilm.js
@@ -89,7 +89,7 @@ export default styles = StyleSheet.create({
     watchbutton: {
         position: 'absolute',
         right: 10,
-        bottom: 0,
+        bottom: -5,
         width: 100,
         height: 30,
         flexDirection: 'row',
