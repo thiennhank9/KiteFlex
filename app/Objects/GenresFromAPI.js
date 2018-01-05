c129bc24e44aaa08a5b8523db752134e1e1fedbe
nhan <thiennhank9@gmail.com>
2018-01-05 20:02:03 +0700

    update icon category

--- app/Objects/GenresFromAPI.js
+++ app/Objects/GenresFromAPI.js
@@ -3,7 +3,7 @@ export default genres = [
         key: 1,
         id: 28,
         name: "Action",
-        icon_name: 'approval'
+        icon_name: 'ticket'
     },
     {
         key: 2,
@@ -15,96 +15,96 @@ export default genres = [
         key: 3,
         id: 16,
         name: "Animation",
-        icon_name: 'approval'
+        icon_name: 'animation'
     },
     {
         key: 4,
         id: 35,
         name: "Comedy",
-        icon_name: 'approval'
+        icon_name: 'apple-keyboard-command'
     },
     {
         key: 5,
         id: 80,
         name: "Crime",
-        icon_name: 'approval'
+        icon_name: 'creation'
     },
     {   key: 6,
         id: 99,
         name: "Documentary",
-        icon_name: 'approval'
+        icon_name: 'file-document'
     },
     {
         key: 7,
         id: 18,
         name: "Drama",
-        icon_name: 'approval'
+        icon_name: 'image-filter-drama'
     },
     {
         key: 8,
         id: 10751,
         name: "Family",
-        icon_name: 'approval'
+        icon_name: 'heart'
     },
     {
         key: 9,
         id: 14,
         name: "Fantasy",
-        icon_name: 'approval'
+        icon_name: 'fan'
     },
     {
         key: 10,
         id: 36,
         name: "History",
-        icon_name: 'approval'
+        icon_name: 'pillar'
     },
     {
         key: 11,
         id: 27,
         name: "Horror",
-        icon_name: 'approval'
+        icon_name: 'ghost'
     },
     {
         key: 12,
         id: 10402,
         name: "Music",
-        icon_name: 'approval'
+        icon_name: 'bookmark-music'
     },
     {
         key: 13,
         id: 9648,
         name: "Mystery",
-        icon_name: 'approval'
+        icon_name: 'crosshairs-gps'
     },
     {
         key: 14,
         id: 10749,
         name: "Romance",
-        icon_name: 'approval'
+        icon_name: 'heart'
     },
     {
         key: 15,
         id: 878,
         name: "Science Fiction",
-        icon_name: 'approval'
+        icon_name: 'office'
     },
     {
         key: 16,
         id: 10770,
         name: "TV Movie",
-        icon_name: 'approval'
+        icon_name: 'cctv'
     },
     {
         key: 17,
         id: 53,
         name: "Thriller",
-        icon_name: 'approval'
+        icon_name: 'feather'
     },
     {
         key: 18,
         id: 10752,
         name: "War",
-        icon_name: 'approval'
+        icon_name: 'alert-octagram'
     },
     {
         key: 19,
